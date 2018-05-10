a4926e5f7b3d7278a00d91fe3af44e946083d7aa
snh7 <35547411+snh7@users.noreply.github.com>
2018-05-10 04:29:45 -0400

    added rest of view files

--- dashboard/index.php
+++ dashboard/index.php
@@ -4,3 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 date_default_timezone_set('America/New_York');
 session_start();
+
+if(!isset($_SESSION['email'])) {
+    header('Location: ../login');
+}
---
+++ views/register.php
@@ -0,0 +1,7 @@
+<?php include 'header.php'; ?>
+    <div class="container mt-5">
+      <?php include 'flashmessages.php' ?>
+      <div class="card card-register">
+        <h4 class="card-header">
+          Sign Up
+        </h4>

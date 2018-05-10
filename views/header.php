cea4b770b80325fd36d2c249dcf3d24cabc7326d
snh7 <35547411+snh7@users.noreply.github.com>
2018-05-10 04:19:44 -0400

    started nav

--- views/header.php
+++ views/header.php
@@ -10,7 +10,17 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    
+
     <link href="https://web.njit.edu/~dg94/is218/todo/assets/css/main.css" rel="stylesheet">
     <title>Todo App</title>
   </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+
+      <a class="navbar-brand" href="https://web.njit.edu/~dg94/is218/todo/">My Todo App</a>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto"></ul>
+
